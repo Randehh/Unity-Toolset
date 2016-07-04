5be49fab348f2822805328107e77acd428868234
Randy Schouten <randyschouten@outlook.com>
2016-07-05 00:28:56 +0200

    new file:   Editor_Extensions_Project/Assets/Editors.meta 	new file:   Editor_Extensions_Project/Assets/Editors/AssetEditorUtils.cs 	new file:   Editor_Extensions_Project/Assets/Editors/AssetEditorUtils.cs.meta 	new file:   Editor_Extensions_Project/Assets/Editors/AssetManager.cs 	new file:   Editor_Extensions_Project/Assets/Editors/AssetManager.cs.meta 	new file:   Editor_Extensions_Project/Assets/Editors/IAssetEditor.cs 	new file:   Editor_Extensions_Project/Assets/Editors/IAssetEditor.cs.meta 	new file:   Editor_Extensions_Project/Assets/Editors/InspectorAssetEditor.meta 	new file:   Editor_Extensions_Project/Assets/Editors/InspectorAssetEditor/InspectorAssetEditor.cs 	new file:   Editor_Extensions_Project/Assets/Editors/InspectorAssetEditor/InspectorAssetEditor.cs.meta 	new file:   Editor_Extensions_Project/Assets/Editors/TableEditor.meta 	new file:   Editor_Extensions_Project/Assets/Editors/TableEditor/BaseTable.cs 	new file:   Editor_Extensions_Project/Assets/Editors/TableEditor/BaseTable.cs.meta 	new file:   Editor_Extensions_Project/Assets/Editors/TableEditor/TableColumn.cs 	new file:   Editor_Extensions_Project/Assets/Editors/TableEditor/TableColumn.cs.meta 	new file:   Editor_Extensions_Project/Assets/Examples.meta 	new file:   Editor_Extensions_Project/Assets/Examples/EditorAsset.cs 	new file:   Editor_Extensions_Project/Assets/Examples/EditorAsset.cs.meta 	new file:   Editor_Extensions_Project/Assets/Examples/Inspector.meta 	new file:   Editor_Extensions_Project/Assets/Examples/Inspector/TestInspector.cs 	new file:   Editor_Extensions_Project/Assets/Examples/Inspector/TestInspector.cs.meta 	new file:   Editor_Extensions_Project/Assets/Examples/Inspector/TestInspectorEditor.cs 	new file:   Editor_Extensions_Project/Assets/Examples/Inspector/TestInspectorEditor.cs.meta 	new file:   Editor_Extensions_Project/Assets/Examples/Table.meta 	new file:   Editor_Extensions_Project/Assets/Examples/Table/TestTable.cs 	new file:   Editor_Extensions_Project/Assets/Examples/Table/TestTable.cs.meta 	new file:   Editor_Extensions_Project/Assets/Extensions.meta 	new file:   Editor_Extensions_Project/Assets/Extensions/TransformExtension.cs 	new file:   Editor_Extensions_Project/Assets/Extensions/TransformExtension.cs.meta 	new file:   Editor_Extensions_Project/ProjectSettings/AudioManager.asset 	new file:   Editor_Extensions_Project/ProjectSettings/ClusterInputManager.asset 	new file:   Editor_Extensions_Project/ProjectSettings/DynamicsManager.asset 	new file:   Editor_Extensions_Project/ProjectSettings/EditorBuildSettings.asset 	new file:   Editor_Extensions_Project/ProjectSettings/EditorSettings.asset 	new file:   Editor_Extensions_Project/ProjectSettings/GraphicsSettings.asset 	new file:   Editor_Extensions_Project/ProjectSettings/InputManager.asset 	new file:   Editor_Extensions_Project/ProjectSettings/NavMeshAreas.asset 	new file:   Editor_Extensions_Project/ProjectSettings/NetworkManager.asset 	new file:   Editor_Extensions_Project/ProjectSettings/Physics2DSettings.asset 	new file:   Editor_Extensions_Project/ProjectSettings/ProjectSettings.asset 	new file:   Editor_Extensions_Project/ProjectSettings/ProjectVersion.txt 	new file:   Editor_Extensions_Project/ProjectSettings/QualitySettings.asset 	new file:   Editor_Extensions_Project/ProjectSettings/TagManager.asset 	new file:   Editor_Extensions_Project/ProjectSettings/TimeManager.asset 	new file:   Editor_Extensions_Project/ProjectSettings/UnityAdsSettings.asset 	new file:   Editor_Extensions_Project/ProjectSettings/UnityConnectSettings.asset

---
+++ Editor_Extensions_Project/Assets/Examples/Table/TestTable.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+using Rondo.Editor.Example;
+
+namespace Rondo.Editor.Table.Example {
+    public class TestTable : BaseTable<EditorAsset> {
+
+        //Keep a reference to the window
+        private static TestTable instance = null;
+
+        //Add a menu item for the table
+        [MenuItem("Tables/Sample Table")]
+        static void OpenEditor() {
+
+            //Get the first window we can find of this type
+            TestTable window = (TestTable)GetWindow(typeof(TestTable));
+
+            //Change window title
+            window.titleContent = new GUIContent("Sample Table");
+
+            //Change file location
+            window.FileLocation = "Data/Sample_Data_Table";
+
+            //Set editable fields with TableColumns
+            window.SetEditableFields(
+                new TableColumn("fileName", 150),
+                new TableColumn("sprite", 150),
+                new TableColumn("health", 50),
+                new TableColumn("movespeed", 50),
+                new TableColumn("mana", 50));
+
+            //Load all assets now that we set all the options
+            window.AssetManager.LoadAssets();
+
+            //Display the window
+            window.Show();
+
+            //Keep a reference to the window
+            instance = window;
+        }
+    }
+}
